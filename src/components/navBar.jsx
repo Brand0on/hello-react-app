5234a7e215a475180bf6bb86aa3d29a469f39915
Brandon Alcalde <brandonalcalde1999@gmail.com>
2022-11-17 17:08:23 +0000

    trying to fix the images

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -2,6 +2,7 @@ function fotos() {
   return (
     <div>
       <img src="./../images/ironhack-logo-xs.png" alt="Ironhack logo" />
+      <img src="./../images/menu-top-xs.png" alt="Menu logo" />
     </div>
   );
 }
